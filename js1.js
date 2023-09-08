c7a31cf3e076d32e49c4f0187f1606768f763c57
Lonewolfonrock <110655331+Lonewolfonrock@users.noreply.github.com>
2023-09-08 19:07:31 +0545

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,28 @@
+let U=0;
+let G=0;
+
+
+function Go1(){
+    U+=1;
+    document.getElementById("Scores").innerHTML=U;
+}
+function Go2(){
+    U+=2;
+    document.getElementById("Scores").innerHTML=U;
+}
+function Go3(){
+    U+=3;
+    document.getElementById("Scores").innerHTML=U;
+}
+function GoG1(){
+    G+=1;
+    document.getElementById("Score").innerHTML=G;
+}
+function GoG2(){
+    G+=2;
+    document.getElementById("Score").innerHTML=G;
+}
+function GoG3(){
+    G+=3;
+    document.getElementById("Score").innerHTML=G;
+}
